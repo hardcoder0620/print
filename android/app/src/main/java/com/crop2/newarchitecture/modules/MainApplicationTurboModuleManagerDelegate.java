6168c4f5c9dd4350102635d400277527f575f728
rushikesh pingle <thehardcoder0620@gmail.com>
2023-01-19 22:08:06 +0530

    ds

--- android/app/src/main/java/com/crop2/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/crop2/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -41,7 +41,7 @@ public class MainApplicationTurboModuleManagerDelegate
     if (!sIsSoLibraryLoaded) {
       // If you change the name of your application .so file in the Android.mk file,
       // make sure you update the name here as well.
-      SoLoader.loadLibrary("crop2_appmodules");
+      SoLoader.loadLibrary("ecompusell_appmodules");
       sIsSoLibraryLoaded = true;
     }
   }
